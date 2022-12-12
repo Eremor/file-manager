1b7feb3bafb9b9ed6b890f54bae1d959c5bdbc2c
Eremor <artem.s13@gmail.com>
2022-12-12 10:15:21 +0300

    feat: add show home directory

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import readline from 'readline/promises';
 import { stdin, stdout } from 'process';
+import { homedir } from 'os';
 import { getUsername } from './utils/utils.js';
 
 const rl = readline.createInterface({
@@ -7,31 +8,44 @@ const rl = readline.createInterface({
   output: stdout,
 });
 
-// const username = argv.splice(2)[0].split('=')[1];
 const username = getUsername();
+const currentDir = homedir();
 
-const commands = {
-  '.exit': () => rl.close(),
-  'add': (line) => console.log('create file', line)
-}
+// const commands = {
+//   '.exit': () => rl.close(),
+//   'add': (line) => console.log('create file', line)
+// }
 
 stdout.write(`Welcome to the File Manager, ${username}! \n`);
+stdout.write(`You are currently in ${currentDir}! \n`);
 
 rl.on('line', (line) => {
   const command = line.split(' ')[0];
 
-  const arr = Object.keys(commands);
-  for (let key of arr) {
-    if (command == key) {
-      commands[command].call(this, line);
-    }
-  }
+  // const arr = Object.keys(commands);
+  // for (let key of arr) {
+  //   if (command == key) {
+  //     commands[command].call(this, line);
+  //   }
+  // }
 
-  const error = arr.filter((item) => item === command);
+  // const error = arr.filter((item) => item === command);
 
-  if(error.length === 0) {
-    console.log('Invalid input');
+  // if(error.length === 0) {
+  //   console.log('Invalid input');
+  // }
+  switch (command) {
+    case '.exit':
+      rl.close();
+      break;
+    case 'add':
+      stdout.write(`create file: ${line} \n`);
+      break;
+  
+    default:
+      stdout.write('Invalid input \n');
+      break;
   }
 });
 
-rl.on('close', () => console.log(`Thank you for using File Manager, ${username}, goodbye!`));
+rl.on('close', () => stdout.write(`Thank you for using File Manager, ${username}, goodbye!`));
