9c0f301ff926aa38e5e9db45e686b52e097a0065
Eremor <artem.s13@gmail.com>
2022-12-16 15:57:12 +0300

    refactor: add line parser

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readline from 'readline/promises';
 import { stdin, stdout } from 'process';
 import { homedir } from 'os';
 
-import { getUsername } from './utils/utils.js';
+import { getUsername, lineParser } from './utils/utils.js';
 import { upDir } from './nwd/up.js';
 import { changeDir } from './nwd/cd.js';
 import { showDir } from './nwd/ls.js';
@@ -23,32 +23,16 @@ const rl = readline.createInterface({
 const username = getUsername();
 let currentDir = homedir();
 
-// const commands = {
-//   '.exit': () => rl.close(),
-//   'add': (line) => console.log('create file', line)
-// }
-
 stdout.write(`Welcome to the File Manager, ${username}! \n\n`);
 stdout.write(`You are currently in ${currentDir}! \n`);
 
 rl.prompt();
 
 rl.on('line', async (line) => {
+  const [cmd, args] = lineParser(line);
+
   const command = line.split(' ')[0];
 
-  // const arr = Object.keys(commands);
-  // for (let key of arr) {
-  //   if (command == key) {
-  //     commands[command].call(this, line);
-  //   }
-  // }
-
-  // const error = arr.filter((item) => item === command);
-
-  // if(error.length === 0) {
-  //   console.log('Invalid input');
-  // }
-  
   try {
     switch (command) {
       case '.exit':
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,11 @@
 import { argv } from 'process';
 
+const quotes = {
+  single: '\'',
+  double: '\"',
+  none: ' '
+}
+
 export const getUsername = () => {
   let username = 'Anonymous';
   const arrArgs = argv.splice(2);
@@ -12,4 +18,27 @@ export const getUsername = () => {
   }
 
   return username;
+}
+
+export const lineParser = (line) => {
+  let sep = ' ';
+
+  if (line.includes(quotes.single)) {
+    sep = quotes.single;
+  } else if (line.includes(quotes.double)) {
+    sep = quotes.double;
+  } else {
+    sep = quotes.none;
+  }
+
+  return extractArgs(line, sep);
+}
+
+const extractArgs = (line, sep) => {
+  const arr = line.split(sep);
+  const args = arr.filter((item) => item.trim().length > 0);
+  const command = args.shift().trim();
+  const argsAsArray = args;
+
+  return [command, argsAsArray]
 }
